da6fc01d68c51233518facf7120374865ab63ca0
Joe Wingbermuehle <joewing@joewing.net>
2013-04-17 15:29:15 -0500

    Top-level module.

--- src/memgen.adb
+++ src/memgen.adb
@@ -29,7 +29,33 @@ begin
    RAM.Register(gen);
    SPM.Register(gen);
 
+   Put_Line("module memory(clk, rst, addr, din, dout, re, we, ready);");
+   New_Line;
+   Put_Line("   input wire clk;");
+   Put_Line("   input wire rst;");
+   Put_Line("   input wire [63:0] addr;");
+   Put_Line("   input wire [63:0] din;");
+   Put_Line("   output wire [63:0] dout;");
+   Put_Line("   input wire re;");
+   Put_Line("   input wire we;");
+   Put_Line("   output wire ready;");
+   New_Line;
    Put_Line(Generate(gen, mem, 64, 64));
+   declare
+      id       : constant Natural   := Get_ID(mem.all);
+      id_str   : constant String    := Natural'Image(id);
+      name     : constant String
+                  := "m" & id_str(id_str'First + 1 .. id_str'Last);
+   begin
+      Put_Line("   assign " & name & "_addr = addr;");
+      Put_Line("   assign " & name & "_din = din;");
+      Put_Line("   assign dout = " & name & "_dout;");
+      Put_Line("   assign " & name & "_re = re;");
+      Put_Line("   assign " & name & "_we = we;");
+      Put_Line("   assign ready = " & name & "_ready;");
+   end;
+   New_Line;
+   Put_Line("endmodule");
    Destroy(mem);
 
 end MemGen;
