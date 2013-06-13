0050acb4b53457eb74b2107312b8d9b35b3850d8
Joe Wingbermuehle <joewing@joewing.net>
2013-06-13 14:14:57 -0500

    Update Test.RAM.

--- src/test-ram.adb
+++ src/test-ram.adb
@@ -6,6 +6,7 @@ package body Test.RAM is
    procedure Run_Tests is
 
       mem : RAM_Pointer := Create_RAM(latency      => 100,
+                                      burst        => 1,
                                       word_size    => 4,
                                       word_count   => 8);
       other : Memory_Pointer := null;
@@ -21,26 +22,26 @@ package body Test.RAM is
       Check(Get_Writes(mem.all) = 0);
 
       Read(mem.all, 2, 4);
-      Check(Get_Time(mem.all) = 300);
+      Check(Get_Time(mem.all) = 201);
       Check(Get_Writes(mem.all) = 0);
 
       Read(mem.all, 2, 2);
-      Check(Get_Time(mem.all) = 400);
+      Check(Get_Time(mem.all) = 301);
       Check(Get_Writes(mem.all) = 0);
 
       Read(mem.all, 2, 3);
-      Check(Get_Time(mem.all) = 600);
+      Check(Get_Time(mem.all) = 402);
       Check(Get_Writes(mem.all) = 0);
 
       Write(mem.all, 1, 8);
-      Check(Get_Time(mem.all) = 900);
+      Check(Get_Time(mem.all) = 504);
       Check(Get_Writes(mem.all) = 1);
 
       Reset(mem.all);
       Check(Get_Time(mem.all) = 0);
 
       Check(To_String(mem.all) =
-            "(ram (latency 100)(word_size 4)(word_count 8))");
+            "(ram (latency 100)(burst 1)(word_size 4)(word_count 8))");
 
       other := Clone(mem.all);
       Check(To_String(other.all) = To_String(mem.all));
